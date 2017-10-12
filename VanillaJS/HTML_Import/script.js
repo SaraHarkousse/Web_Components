e11839729e690c97c91268714472d15187eda8e4
SaraHarkousse <sara.harkousse@gmail.com>
2017-10-12 11:05:46 +0200

    first commit

--- VanillaJS/HTML_Import/script.js
+++ VanillaJS/HTML_Import/script.js
@@ -2,7 +2,7 @@ var link = document.querySelector('link[rel="import"]');
 var content = link.import;
 
 // Grab DOM from component.html's document.
-var template = content.querySelector('#template');
-var clone = template.content.cloneNode(true);
+var el = content.querySelector('#container');
+var clone = el.cloneNode(true);
 var host = document.querySelector('#host');
 host.appendChild(clone);
